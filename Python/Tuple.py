324a404cf7ebbecf96adf13aa4de36a9cf8b14cc
Muk74dir <muktadir.sarker99@gmail.com>
2022-05-21 03:49:47 +0600

    All Test Cases Passed

--- Python/Tuple.py
+++ Python/Tuple.py
@@ -1,5 +1,7 @@
 n = int(input())
+val = input().split(" ")
 result = []
-for i in range(1, n+1):
-    result.append(i)
+for i in range(n):
+    result.append(int(val[i]))
+print(result)
 print(hash(tuple(result)))
