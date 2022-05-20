f57b25998eebf0c0bd1092df405673dabc17adab
Muk74dir <muktadir.sarker99@gmail.com>
2022-05-21 04:37:37 +0600

    All Test Cases Passed

---
+++ Finding the percentage.py
@@ -0,0 +1,12 @@
+n = int(input())
+data = {}
+avar = 0
+for i in range(n):
+    name, *val = input().split()
+    scores = list(map(float, val))
+    data[name] = scores
+select = input()
+for i, j in data.items():
+    if select == i:
+        avar = sum(j) / len(j)
+print('%.2f' %avar)
